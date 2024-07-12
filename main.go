b9882562cf5abd5b68934d3d9d43419a4ea16aca
Vladimir Kidyaev <krios2146@gmail.com>
2024-07-12 17:46:30 +0700

    feat: update main with manual setting of entities and rendering

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"github.com/krios2146/simulation-go/internal/coordinates"
+	"github.com/krios2146/simulation-go/internal/entities"
+	"github.com/krios2146/simulation-go/internal/ui"
+	"github.com/krios2146/simulation-go/internal/world"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	worldMap := world.NewMap(10, 10)
+	var entity entities.Entity
+	var coordinates coordinates.Coordinates
+
+	coordinates.X = 1
+	coordinates.Y = 1
+	entity = entities.Rock{Coordinates: coordinates}
+
+	worldMap.Add(entity, coordinates)
+
+	coordinates.X = 2
+	coordinates.Y = 2
+	entity = entities.Grass{Coordinates: coordinates}
+
+	worldMap.Add(entity, coordinates)
+
+	coordinates.X = 8
+	coordinates.Y = 6
+	entity = entities.Herbivore{Coordinates: coordinates}
+
+	worldMap.Add(entity, coordinates)
+
+	coordinates.X = 6
+	coordinates.Y = 8
+	entity = entities.Predator{Coordinates: coordinates}
+
+	worldMap.Add(entity, coordinates)
+
+	ui.Render(*worldMap)
 }
