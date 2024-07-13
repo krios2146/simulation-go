827edc4929eb6e04a6b2f3c3dac5a9916c5706e9
Vladimir Kidyaev <krios2146@gmail.com>
2024-07-14 03:57:30 +0700

    refacotor: delete initactions.go & add `SpawnAction` struct

--- internal/actions/actions.go
+++ internal/actions/actions.go
@@ -1,7 +1,65 @@
 package actions
 
-import "github.com/krios2146/simulation-go/internal/world"
+import (
+	"math/rand"
+
+	"github.com/krios2146/simulation-go/internal"
+	"github.com/krios2146/simulation-go/internal/entities"
+	"github.com/krios2146/simulation-go/internal/pathfinder"
+	"github.com/krios2146/simulation-go/internal/world"
+)
 
 type Action interface {
 	Perform(m *world.Map)
+}
+
+type SpawnAction[T entities.Entity] struct {
+	amount uint8
+}
+
+type TurnAction struct {
+	pathFinder pathfinder.PathFinder
+}
+
+func NewSpawnAction[T entities.Entity](t T, amount uint8) *SpawnAction[T] {
+	return &SpawnAction[T]{
+		amount: amount,
+	}
+}
+
+func (sa SpawnAction[T]) Perform(m *world.Map) {
+	var x, y int
+	var randCoordinates internal.Coordinates
+
+	for i := 0; i < int(sa.amount); {
+		x = rand.Intn(int(m.Width))
+		y = rand.Intn(int(m.Height))
+
+		randCoordinates = internal.Coordinates{X: uint8(x), Y: uint8(y)}
+
+		if m.IsEmpty(randCoordinates) {
+			entity := T.New(*new(T), randCoordinates)
+			m.Add(entity, randCoordinates)
+			i++
+		}
+	}
+}
+
+func NewTurnAction() *TurnAction {
+	return &TurnAction{
+		pathFinder: &pathfinder.BFSPathFinder{},
+	}
+}
+
+func (ta TurnAction) Perform(m *world.Map) {
+	creatures := world.FindByType(*new(entities.Creature), m)
+
+	for _, c := range creatures {
+		path := ta.pathFinder.FindPath(*m, c, c.InteractsWith())
+		makeMove(path, m, c)
+	}
+}
+
+func makeMove(path []internal.Coordinates, m *world.Map, creature entities.Creature) {
+	panic("`makeMove` not implemented")
 }
